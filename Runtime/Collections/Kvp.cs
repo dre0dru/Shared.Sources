76486a9d2f45c06f258a9db36874a6c9e8ee6a15
Dre0Dru <dre0druofficial@gmail.com>
2021-12-17 22:52:00 +0300

    feat: exposed dictionary serialization, better constant string drawer
    BREAKING CHANGE: renamed serialized fields for Kvp

--- Runtime/Collections/Kvp.cs
+++ Runtime/Collections/Kvp.cs
@@ -3,13 +3,32 @@ using UnityEngine;
 
 namespace Shared.Sources.Collections
 {
+    public interface IKvp<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+
+        public TValue Value { get; set; }
+    }
+    
     [Serializable]
-    public struct Kvp<TKey, TValue>
+    public struct Kvp<TKey, TValue> : IKvp<TKey, TValue>
     {
         [SerializeField]
-        public TKey Key;
+        private TKey _key;
 
         [SerializeField]
-        public TValue Value;
+        private TValue _value;
+
+        public TKey Key
+        {
+            get => _key;
+            set => _key = value;
+        }
+
+        public TValue Value
+        {
+            get => _value;
+            set => _value = value;
+        }
     }
 }
