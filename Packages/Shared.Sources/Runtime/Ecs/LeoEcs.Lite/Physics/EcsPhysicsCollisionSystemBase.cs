9c9a4282aa47adf8add7cc1211d7780aecd946dc
Dre0Dru <dre0druofficial@gmail.com>
2022-03-26 13:45:18 +0300

    feat: folder symlinks disabled by default, ecs physics system receives subscriber in ctor
    chore: unity version and packages update

--- Packages/Shared.Sources/Runtime/Ecs/LeoEcs.Lite/Physics/EcsPhysicsCollisionSystemBase.cs
+++ Packages/Shared.Sources/Runtime/Ecs/LeoEcs.Lite/Physics/EcsPhysicsCollisionSystemBase.cs
@@ -5,23 +5,18 @@ using MessagePipe;
 
 namespace Shared.Sources.Ecs.LeoEcs.Lite.Physics
 {
-    public abstract class EcsPhysicsCollisionSystemBase : IEcsInitSystem, IEcsDestroySystem
+    public abstract class EcsPhysicsCollisionSystemBase : IEcsDestroySystem
     {
         protected readonly Queue<CollisionEvent> _collisionEvents;
-
-        private IDisposable _collisionSubscriber;
+        private readonly IDisposable _collisionSubscriber;
         
         #if UNITY_2020_3_OR_NEWER
         [UnityEngine.Scripting.RequiredMember]
         #endif
-        protected EcsPhysicsCollisionSystemBase()
+        protected EcsPhysicsCollisionSystemBase(ISubscriber<CollisionEvent> collisionEventsSubscriber)
         {
             _collisionEvents = new Queue<CollisionEvent>();
-        }
-
-        public virtual void Init(EcsSystems systems)
-        {
-            _collisionSubscriber = GlobalMessagePipe.GetSubscriber<CollisionEvent>().Subscribe(OnCollisionEvent);
+            _collisionSubscriber = collisionEventsSubscriber.Subscribe(OnCollisionEvent);
         }
 
         public virtual void Destroy(EcsSystems systems)
