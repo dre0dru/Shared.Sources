327ae765a543918701198368b33f1c3a79eee9f4
Dre0Dru <dre0druofficial@gmail.com>
2022-06-03 23:27:28 +0300

    feat: service locator vcontainer extensions, serializable flattened array

--- Runtime/IoC/ServiceLocators/Service.cs
+++ Runtime/IoC/ServiceLocators/Service.cs
@@ -1,14 +1,27 @@
+using System;
 
 namespace Shared.Sources.IoC.ServiceLocators
 {
-    public class Service<T> where T : class
+    public class Service<T>
+        where T : class
     {
         private static T _instance;
 
         public static T Value
         {
             get => _instance;
+            [Obsolete("Use Set(T)", true)]
             set => _instance = value;
+        }
+
+        public static void Set(T value)
+        {
+            _instance = value;
+        }
+
+        public static void Clear()
+        {
+            _instance = default(T);
         }
     }
 }
